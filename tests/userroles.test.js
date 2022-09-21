458379fc0e07a24aae9d14358d56c99eaa180045
Martin Fallenstedt <martin.fallenstedt@gmail.com>
2022-09-21 13:38:53 +0200

    Added unit test for roles association

--- tests/userroles.test.js
+++ tests/userroles.test.js
@@ -9,64 +9,118 @@ const userInfo = {
     password: "samplepassword"
 }
 
-let roleArray;
+let roleArray = [];
+let createdUserId = 0;
 
 describe('/api/userroles', () => {
-
     beforeAll( async () => {
         server = require ('../index');
-        await createOneUser();
-        createRoles(3);
-
-        //authToken = await user.generateAuthToken();
-        //console.log(`Global Auth Token: ${authToken}`);
+        await createRoles(3);
     });
 
     afterAll( async () => {
-
+        await Role.deleteMany();
+        server.close();
     });
 
-    beforeEach(async () => {
-        server.close();
-
-        // Clean up database
-        await Role.deleteMany();
-        await User.deleteMany();
+    beforeEach( async () => {
+        await createOneUser();
     });
 
     afterEach(async () => {
-
+        await User.deleteMany();
     });
+
+    // -----------------------------------------------------------------------
+    // -----------------------------------------------------------------------
 
     describe('POST /', () => {
         it('should associate one role with a user', async () => {
-            let res = await request(server)
-                .post('/api/userroles')
-                .query({'userid': createdUserId})
-                .query({'roleid': createdRoleId});
-
-            expect(res.status).toBe(200);
+            await associateOneRoleWithUser();
 
             res = await request(server)
                 .post('/api/userroles')
                 .query({'userid': createdUserId})
-                .query({'roleid': createdRoleId});
+                .query({'roleid': roleArray[0]._id});
 
             expect(res.status).toBe(400);
         });
 
         it('should associate several roles with a user', async () => {
-            let roleIdList = createRoles(3);
+            await associateManyRolesWithUser();
+        });
+    });
 
-            let res = await request(server)
-                .post('/api/userroles')
-                .query({'userid': createdUserId})
-                .query({'roleid': roleIdList});
+    describe('GET /', () => {
+        it('should validate that roles are associated with a user', async () => {
+            let userObj = await associateManyRolesWithUser();
 
-            expect(res.status).toBe(200);
+            expect(userObj.roles.length).toBe(3);
+            expect(userObj.roles[0]).toBe(roleArray[0]._id);
+            expect(userObj.roles[1]).toBe(roleArray[1]._id);
+            expect(userObj.roles[2]).toBe(roleArray[2]._id);
+        });
+    });
+
+    describe('DELETE /', () => {
+        it('should validate that roles are removed from a user', async () => {
+            let userObj = await associateManyRolesWithUser();
+
+            expect(userObj.roles.length).toBe(3);
+
+            userObj = await removeOneRoleFromUser(roleArray[2]._id);
+            expect(userObj.roles.length).toBe(2);
+            expect(userObj.roles[0]).toBe(roleArray[0]._id);
+            expect(userObj.roles[1]).toBe(roleArray[1]._id);
         });
     });
 });
+
+// ---------------------------------------------------------------
+
+async function removeOneRoleFromUser(roleId) {
+    let res = await request(server)
+        .delete('/api/userroles')
+        .query({'userid': createdUserId})
+        .query({'roleid': roleId});
+
+    expect(res.status).toBe(200);
+    return res.body;
+}
+
+// ---------------------------------------------------------------
+
+async function associateOneRoleWithUser() {
+    let res = await request(server)
+        .post('/api/userroles')
+        .query({'userid': createdUserId})
+        .query({'roleid': roleArray[0]._id});
+
+    expect(res.status).toBe(200);
+}
+
+// ---------------------------------------------------------------
+
+async function associateManyRolesWithUser() {
+    let roleIdList = "";
+    
+    for (let i = 0; i < roleArray.length; i++) {
+        if (roleIdList.length > 0) {
+            roleIdList += ",";
+        }
+
+        roleIdList += roleArray[i]._id;
+    }
+
+    let res = await request(server)
+        .post('/api/userroles')
+        .query({'userid': createdUserId})
+        .query({'roleid': roleIdList});
+
+    expect(res.status).toBe(200);
+    return res.body;
+}
+
 
 // ---------------------------------------------------------------
 
@@ -80,16 +134,6 @@ async function createOneUser() {
     expect(res.body).toHaveProperty('email', userInfo.email);
 
     createdUserId = res.body._id;
-}
-
-// ---------------------------------------------------------------
-
-async function createOneRole() {
-    res = await request(server)
-        .post('/api/roles')
-        .send(roleInfo);
-
-    createdRoleId = res.body._id;
 }
 
 // ---------------------------------------------------------------
@@ -115,6 +159,4 @@ async function createRoles(numRoles) {
             throw `createRoles() error ${res.status}`;
         }
     }
-
-    return roleIdList;
 }
